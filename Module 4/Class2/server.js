c5f9cd5be794cd4db364e51633c34e070f1ab8b0
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2023-08-26 02:43:31 +0530

    server create, Middlewware and endpoint concept

--- Module 4/Class2/server.js
+++ Module 4/Class2/server.js
@@ -4,20 +4,33 @@ const app = express();
 
 
 // I want to make a logg
-app.use((req,res,next)=>{
-    console.log(req.method,req.ip,req.hostname,new Date());
-    next(); 
-})
+// app.use((req,res,next)=>{
+//     console.log(req.method,req.ip,req.hostname,new Date());
+//     next(); 
+// })
+
+const auth = (req,res,next)=>{
+    console.log(req.query)
+    let password=req.query.password;
+    if(password=="123"){
+        next();
+    }
+    else{
+        res.send("you are not authorized person")
+    }
+}
+
+// app.use(auth);  // app.use methos is used to invoke middleware withing whole application
 
 //endpoint
-app.get("/",(req,res)=>{
+app.get("/users",auth,(req,res)=>{
     // res.send("Hello World");
     // res.send("<h1>Hello world in h1 heading</h1")
-    // res.send({"name":"Deepanshu"})
-    res.send({'type':"GET"})
+    res.send({"type":"Ankit"})
+    // res.send({'type':"GET"})
 })
 
-app.post("/",(req,res)=>{
+app.post("/",(req,res)=>{   
     res.send({"type":"POST"});
 })
 
@@ -30,6 +43,6 @@ app.delete("/",(req,res)=>{
 })
 
 app.listen(8082,()=>{
-    console.log("Server has started at 8080")
+    console.log("Server has started at 8082")
 })
 
